0a2fa81086cbca3911ea271bbd8b68f362f92b92
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2024-07-16 16:51:19 +0530

    Setting up roboshop project with Terraform

--- local.tf
+++ local.tf
@@ -1,4 +1,4 @@
 locals {
   private_subnets_route_ids=[module.subnets["web"].route_table_ids,module.subnets["app"].route_table_ids,module.subnets["db"].route_table_ids]
-  all_subnets_route_ids=[module.subnets["web"].route_table_ids,module.subnets["web"].route_table_ids,module.subnets["app"].route_table_ids,module.subnets["db"].route_table_ids]
+  all_subnets_route_ids=[module.subnets["public"].route_table_ids,module.subnets["web"].route_table_ids,module.subnets["app"].route_table_ids,module.subnets["db"].route_table_ids]
 }
